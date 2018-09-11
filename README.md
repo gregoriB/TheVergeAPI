0fddab7f43e6189492c047c8e6537e449c80ee2e
gregoriB <brandon.gregori@gmail.com>
2018-09-11 14:03:59 -0700

    Functional and fully-stylized website with working API

---
+++ README.md
@@ -0,0 +1,17 @@
+Uses an API from https://newsapi.org.
+
+The API key is hidden from the public.  In order to run this, one must add their own API key.  To do this, create an 'apikeys.js' file and add your API key to an object.  Then perform a module export on the object.  Follow this example: 
+
+	const apikey = {
+			SECRET_KEY: 'YOUR_SECRET_API_KEY_GOES_HERE'
+			}
+
+	module.exports = apikey;
+
+
+Of if you choose to, you can instead paste your API key into the 'key' variable in the app.js file.  Example:
+
+	const key = 'YOUR_SECRET_API_KEY_GOES_HERE';
+
+
+It's recommended that you don't upload your API key to github or anywhere else public.  Keep 'gitignore' file to exclude your apikeys.js file from the repository.
---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const apikey = require('./apikeys.js');
+
+const server = app.listen(3000, "127.0.0.1", function () {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log('server running at:');
+    console.log('http://' + host + ':' + port);
+});
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    const url = 'https://newsapi.org/v2/top-headlines?sources=the-verge&';
+    const key = apikey.SECRET_KEY; // taken from exported object in ./apikeys.js
+    request(url + key, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let data = JSON.parse(body);
+            res.render('index', {data: data});
+        } else {
+            console.log(error);
+        } 
+    }); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+   console.log('server started'); 
+});
