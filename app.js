93c1599d6ddaccf5ce2bc2281ef565a1772828e4
gregoriB <brandon.gregori@gmail.com>
2019-01-11 22:12:15 -0800

    cleaned up code a bit, added normalize.css file

--- app.js
+++ app.js
@@ -11,17 +11,12 @@ app.get('/', (req, res) =>  {
     const url = 'https://newsapi.org/v2/top-headlines?sources=the-verge&';
     const key = process.env.API_KEY;
     request(url + key, (error, response, body) => {
-        if (error) {
-            throw error;
-        }
-        if (response.statusCode !== 200) {
-            return res.send(response.statusCode);
-        }
+        if (error) throw error;
+        if (response.statusCode !== 200) return res.send(response.statusCode);
+
         const data = JSON.parse(body);
         res.render('index', {data: data});
     }); 
 });
 
-const listener = app.listen(8080, () => {
-    console.log(`app listening on port: ${listener.address().port}`);
-  });
+const listener = app.listen(8080, () => console.log(`app listening on port: ${listener.address().port}`));
